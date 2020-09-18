451a88687caef97f046dcb7b9669a6cb4711fdbf
Francesco Bedussi <francesco.bedussi@abstract.it>
2020-09-18 16:41:55 +0200

    chore: add demo

---
+++ demo/precache-manifest.0a252a7546d76ced201dbda56f06b4ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d558512867c1e5f376518f489970284",
+    "url": "/5adc6a6833404f43b4fc.worker.js"
+  },
+  {
+    "revision": "05ff9b6cc784e38988bfbaf309410fe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "823fbed51b1d89ede98b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "592ac75ea7704b5a554d",
+    "url": "/static/js/2.81f07a75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.81f07a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823fbed51b1d89ede98b",
+    "url": "/static/js/main.e6fe3195.chunk.js"
+  },
+  {
+    "revision": "de5510aa175980a3d98e",
+    "url": "/static/js/runtime-main.429db5b2.js"
+  },
+  {
+    "revision": "b946c8e627a45da7bbf33016aedb1ade",
+    "url": "/static/media/zbar.wasm.b946c8e6.bin"
+  }
+]);
